34140cb78448f27bc67643a1e629aba5993edf18
Carlos Vinícius Figueiredo de Macedo <carlos.vinicius@nubeliu.com>
2018-09-24 14:25:15 -0300

    Ajuste no model/cnf.go para aceitar parametros

--- showglobalstatus/model/cnf.go
+++ showglobalstatus/model/cnf.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"flag"
 	"os"
 
 	"github.com/go-ini/ini"
@@ -14,28 +15,40 @@ func MyCnf() (valueCnf []string) {
 
 	host = cfg.Section("client").Key("host").Validate(func(in string) string {
 		if len(in) == 0 {
-			return "127.0.0.1"
+			var hostParameter string
+			flag.StringVar(&hostParameter, "h", "127.0.0.1", "specify hostParameter to use.  defaults to 127.0.0.1.")
+			go flag.Parse()
+			return hostParameter
 		}
 		return in
 	})
 	valueCnf = append(valueCnf, host)
 	port = cfg.Section("client").Key("port").Validate(func(in string) string {
 		if len(in) == 0 {
-			return "3306"
+			var portParameter string
+			flag.StringVar(&portParameter, "p", "3306", "specify portParameter to use.  defaults to 3306.")
+			flag.Parse()
+			return portParameter
 		}
 		return in
 	})
 	valueCnf = append(valueCnf, port)
 	user = cfg.Section("client").Key("user").Validate(func(in string) string {
 		if len(in) == 0 {
-			return os.Getenv("USER")
+			var userParameter string
+			flag.StringVar(&userParameter, "u", (os.Getenv("USER")), "specify userParameter to use.  defaults to root.")
+			flag.Parse()
+			return userParameter
 		}
 		return in
 	})
 	valueCnf = append(valueCnf, user)
 	password = cfg.Section("client").Key("password").Validate(func(in string) string {
 		if len(in) == 0 {
-			return "GomariaDB"
+			var passParameter string
+			flag.StringVar(&passParameter, "o", "", "specify port to use.  defaults to .")
+			flag.Parse()
+			return passParameter
 		}
 		return in
 	})
